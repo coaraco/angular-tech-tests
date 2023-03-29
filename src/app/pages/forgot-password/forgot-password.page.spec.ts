d2d15db121590c457b15b201e359b4da5994f23f
AlbertoRoldanUX-code <albe.roldan@outlook.com>
2023-03-29 20:39:57 +0200

    Fix test by importing HttpClientModule

--- src/app/pages/forgot-password/forgot-password.page.spec.ts
+++ src/app/pages/forgot-password/forgot-password.page.spec.ts
@@ -2,23 +2,22 @@ import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 import { ForgotPasswordPage } from "./forgot-password.page";
 import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientModule } from "@angular/common/http"; // Import the HttpClientModule
 
 describe("ForgotPasswordPage", () => {
   let component: ForgotPasswordPage;
   let fixture: ComponentFixture<ForgotPasswordPage>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ForgotPasswordPage],
-        imports: [IonicModule, RouterTestingModule],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ForgotPasswordPage],
+      imports: [IonicModule, RouterTestingModule, HttpClientModule], // Add HttpClientModule to the imports array
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(ForgotPasswordPage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    }),
-  );
+    fixture = TestBed.createComponent(ForgotPasswordPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it("should create", () => {
     expect(component).toBeTruthy();
