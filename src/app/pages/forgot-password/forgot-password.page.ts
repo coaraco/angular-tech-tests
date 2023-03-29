1f2e943bcf161b084910a2cb4001dff5a82e6831
AlbertoRoldanUX-code <albe.roldan@outlook.com>
2023-03-29 20:24:45 +0200

    Add support for for password recovery functionality

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-forgot-password",
@@ -6,5 +8,31 @@ import { Component } from "@angular/core";
   styleUrls: ["./forgot-password.page.scss"],
 })
 export class ForgotPasswordPage {
-  constructor() {}
+  public loading = false;
+  public username: string;
+
+  constructor(private http: HttpClient, private alertController: AlertController) {}
+
+  public async resetPassword(): Promise<void> {
+    this.loading = true;
+    const url = `https://httpbin.org/get?username=${this.username}`;
+    try {
+      const response = await this.http.get(url).toPromise();
+      const alert = await this.alertController.create({
+        header: "Success",
+        message: "Your password has been reset!",
+        buttons: ["OK"],
+      });
+      await alert.present();
+    } catch (error) {
+      const alert = await this.alertController.create({
+        header: "Error",
+        message: "An error occurred while resetting your password. Please try again later.",
+        buttons: ["OK"],
+      });
+      await alert.present();
+    } finally {
+      this.loading = false;
+    }
+  }
 }
