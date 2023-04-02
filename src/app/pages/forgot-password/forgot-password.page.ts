03cad3f21fd7a4b869f82db24d203670f6af7888
Fran <valiente.fco@gmail.com>
2023-04-02 16:43:09 +0200

    forgot password page test done

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -3,8 +3,9 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 import { RootState } from "src/app/store";
-
+import * as FromUser from "src/app/store/user/user.selectors";
 
 @Component({
   selector: "app-forgot-password",
@@ -12,6 +13,7 @@ import { RootState } from "src/app/store";
   styleUrls: ["./forgot-password.page.scss"],
 })
 export class ForgotPasswordPage implements OnInit {
+  public loading$: Observable<boolean> = this.store.select(FromUser.selectLoading);
   public forgotPasswordForm: FormGroup;
 
   constructor(
