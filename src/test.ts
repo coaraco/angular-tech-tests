4ac1ecaeff68d0829e824367fdbe79c6cf95f616
sebastian <sebiqm2@gmail.com>
2023-04-03 16:55:46 +0200

    Prueba realizada por Barboza Sebastian

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,18 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import "zone.js/dist/zone-testing";
-import { getTestBed } from "@angular/core/testing";
+import { TestBed, getTestBed } from "@angular/core/testing";
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from "@angular/platform-browser-dynamic/testing";
+import { HttpClientModule } from "@angular/common/http";
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+beforeEach(() => TestBed.configureTestingModule({
+  imports: [HttpClientModule],
+}));
